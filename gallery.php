b968d670b7addd8f134180683c443438be5b5a06
wirelesseyes <83575957+wirelesseyes@users.noreply.github.com>
2022-04-21 12:29:40 -0700

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="Rich Black Woman" content="Rich Black Woman" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
+    <meta name="author" content="Rich Black Woman" />
+    <meta name="generator" content="Rich Black Woman" />
+    <!-- fontawesome free icon  -->
+    <script src="https://kit.fontawesome.com/1e1adc6a60.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    
+    
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+    <!-- Themefisher Icon font -->
+    <link rel="stylesheet" href="plugins/font-style/style.css" />
+    <!-- bootstrap.min css -->
+    <!-- <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" /> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
+    <link rel="stylesheet" href="https://use.typekit.net/skn8ash.css" />
+    <link rel="stylesheet" href="./style.css" />
+    <!-- main js file  -->
+    <script src="js/Flickity.js"></script>
+    <!-- main css file  -->
+    <link rel="stylesheet" href="style/style.css" />
+    <link rel="stylesheet" href="style/Flickity.css" />
+    <link rel="stylesheet" href="style/image-gallery.css" />
+
+    <title>Gallery || Rich Black Woman</title>
+</head>
+
+<body>
+    <!-- header  -->
+    <section class="top-line flexrow">
+        <div class="social">
+            <a href="tel:+12132694004" title="Telephone" class="ph">
+                <img src="image/about/iconmonstr-phone.svg" />
+                <span>+1213-269-4004</span>
+            </a>
+            <a href="" title="Instagram" class="in">
+                <img src="image/about/iconmonstr-instagram.svg" />
+                <span>Instagram</span>
+            </a>
+            <a href="" title="WhatsApp" class="wa">
+                <img src="image/about/iconmonstr-whatsapp.svg" />
+                <span>WhatsApp</span>
+            </a>
+            <a href="" title="WhatsApp" class="wa">
+                <img src="image/about/iconmonstr-facebook-1-240.png" />
+                <span>Facebook</span>
+            </a>
+            <a href="" title="WhatsApp" class="wa">
+                <img src="image/about/iconmonstr-twitter-1-240.png" />
+                <span>Twitter</span>
+            </a>
+        </div>
+    </section>
+    <nav class="TranskNav">
+        <section class="navigation">
+            <div class="fst_content" id="fst_content">
+                <div class="menu-icon" id="menu-icon">
+                    <!-- <i class="fas "></i> -->
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar" style="width: 15px"></span>
+                    <span class="icon-bar"></span>
+                </div>
+                <div class="logo">
+                    <a href=""><img src="image/logo.png" alt="" /></a>
+                </div>
+
+                <div class="nav-content" id="nav-content">
+                    <ul class="nav-ul" id="nav-ul">
+                        <div class="navMediaLogo">
+                            <center>
+                                <a><img src="image/1.png" alt="Rich Black Woman" style="filter: brightness(2.5)" /></a>
+                            </center>
+                        </div>
+                        <div class="closeNavigation">
+                            <i class="tf-ion-android-close"></i>
+                        </div>
+
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="index.html">About</a>
+                        </li>
+                        <li>
+                            <a href="index.html">Contact Us</a>
+                        </li>
+                        <li>
+                            <a href="index.html">Relax Relate Release Retreat</a>
+                        </li>
+                        <li>
+                            <a href="index.html">Sales Coaching</a>
+                        </li>
+                        <li>
+                            <a href="index.html">Shop RBW Couture</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="navRightSection">
+                    <!-- <div class="search-icon">
+            <span class="fas fa-search" aria-hidden="true"></span>
+        </div> -->
+                    <div class="close-src-bar search-icon">
+                        <!-- <span class="fas fa-search"></span> -->
+                        <span class="tf-ion-ios-search"></span>
+                    </div>
+                    <div class="close-src-bar cancel-icon">
+                        <span class="fas fa-times"></span>
+                    </div>
+                    <form action="" method="GET" class="search-box search-box-1">
+                        <input type="search" class="search-data" name="s" placeholder="Search here" required />
+                        <button type="submit" class="fas fa-search go-icon"></button>
+                    </form>
+                    <div class="card">
+                        <a href=""><i class="tf-ion-ios-cart-outline"></i></a>
+                    </div>
+
+                    <!-- profile indicator  -->
+                    <!-- <div class="profile">
+        <a href=""><i class="tf-profile-male"></i></a>
+        </div> -->
+                </div>
+            </div>
+        </section>
+    </nav>
+
+    <!-- partial:index.partial.html -->
+    <section class="gallerySection">
+        <!-- <section>
+            <h1>Vertical image loop with scroll acceleration with gsap</h1>
+        </section> -->
+
+        <div class="gallery">
+            <div class="col">
+                <div class="image">
+                    <img src="https://images.pexels.com/photos/10324713/pexels-photo-10324713.jpeg?cs=srgb&dl=pexels-taha-samet-arslan-10324713.jpg&fm=jpg?auto=compress&cs=tinysrgb&h=650&w=940%20940w"
+                        alt="" />
+                </div>
+                <div class="image">
+                    <img src="https://images.pexels.com/photos/10533885/pexels-photo-10533885.jpeg?auto=compress&cs=tinysrgb&h=650&w=940%20940w"
+                        alt="" />
+                </div>
+                <div class="image">
+                    <img src="https://images.pexels.com/photos/10253213/pexels-photo-10253213.jpeg?cs=srgb&dl=pexels-beepin-10253213.jpg&fm=jpg?auto=compress&cs=tinysrgb&h=650&w=940%20940w"
+                        alt="" />
+                </div>
+            </div>
+            <div class="col">
+                <div class="image">
+                    <img src="https://images.pexels.com/photos/10050979/pexels-photo-10050979.jpeg?cs=srgb&dl=pexels-%D0%B8%D0%BB%D1%8C%D1%8F-%D0%BF%D0%B0%D1%85%D0%BE%D0%BC%D0%BE%D0%B2-10050979.jpg&fm=jpg?auto=compress&cs=tinysrgb&h=650&w=940%20940w"
+                        alt="" />
+                </div>
+                <div class="image">
+                    <img src="https://images.pexels.com/photos/1128660/pexels-photo-1128660.jpeg?cs=srgb&dl=pexels-nur-andi-ravsanjani-gusma-1128660.jpg&fm=jpg?auto=compress&cs=tinysrgb&h=650&w=940%20940w"
+                        alt="" />
+                </div>
+                <div class="image">
+                    <img src="https://images.pexels.com/photos/9699293/pexels-photo-9699293.jpeg?cs=srgb&dl=pexels-lada-rezantseva-9699293.jpg&fm=jpg?auto=compress&cs=tinysrgb&h=650&w=940%20940w"
+                        alt="" />
+                </div>
+            </div>
+            <div class="col">
+                <div class="image">
+                    <img src="https://images.pexels.com/photos/6405575/pexels-photo-6405575.jpeg?cs=srgb&dl=pexels-daria-sannikova-6405575.jpg&fm=jpg?auto=compress&cs=tinysrgb&h=650&w=940%20940w"
+                        alt="" />
+                </div>
+                <div class="image">
+                    <img src="https://images.pexels.com/photos/10162526/pexels-photo-10162526.jpeg?cs=srgb&dl=pexels-svetlana%F0%9F%8E%9E-10162526.jpg&fm=jpg?auto=compress&cs=tinysrgb&h=650&w=940%20940w"
+                        alt="" />
+                </div>
+                <div class="image">
+                    <img src="https://images.pexels.com/photos/4394807/pexels-photo-4394807.jpeg?cs=srgb&dl=pexels-woodysmedia-4394807.jpg&fm=jpg?auto=compress&cs=tinysrgb&h=650&w=940%20940w"
+                        alt="" />
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="footer-logo-part">
+            <img class="footer-logo" src="image/1.png" alt="logo" />
+            <img class="footer-visaCard" src="image/payment.icon.en.png" alt="visa card" />
+            <div class="phone">
+                <a href="tel:+">+1 213-269-4004</a>
+                <span>caviar@caviar.global</span>
+            </div>
+        </div>
+        <div class="subscribe-part">
+            <p>
+                Enter your email address to receive special offers and promotions.
+            </p>
+            <form action="" method="GET" class="email-input">
+                <input type="email" class="email" name="email" placeholder="Subscribe" required />
+                <button type="submit" class="tf-ion-android-send"></button>
+            </form>
+            <div class="socialIcon">
+                <ul>
+                    <li>
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><i
+                                class="tf-ion-social-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><i
+                                class="tf-ion-social-youtube"></i></a>
+                    </li>
+                    <li>
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><i
+                                class="tf-ion-social-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><i
+                                class="tf-ion-social-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer-press">
+            <h2 class="page-inner-title">THE PRESS ABOUT Black Rich Woman:</h2>
+            <div class="footer-press-img-list">
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/gq.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/der_spiegel.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/esquire.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/finders.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/hindustan_times.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/hyperbeast.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/letsgodigital.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/mashable.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/robb_report.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/slash_gear.png"
+                        data-loaded="true" /></a>
+                <a href=""><img class="lozad" data-src="image/footer" src="image/footer/the_verge.png"
+                        data-loaded="true" /></a>
+            </div>
+        </div>
+        <div class="content-option">
+            <div class="footer-section-1">
+                <ul class="part">
+                    <li>
+                        <h3>Quick links</h3>
+                    </li>
+                    <li>
+                        <a href="">About Us</a>
+                    </li>
+                    <li>
+                        <a href="">Returns And Cancellation</a>
+                    </li>
+                    <li>
+                        <a href="">Sales Coaching</a>
+                    </li>
+                    <li>
+                        <a href="">Speaker/Moderator</a>
+                    </li>
+                    <li>
+                        <a href="">Privacy Policy</a>
+                    </li>
+                </ul>
+                <ul class="part">
+                    <li>
+                        <h3>Quick links</h3>
+                    </li>
+                    <li>
+                        <a href="">About Us</a>
+                    </li>
+                    <li>
+                        <a href="">Returns And Cancellation</a>
+                    </li>
+                    <li>
+                        <a href="">Sales Coaching</a>
+                    </li>
+                    <li>
+                        <a href="">Speaker/Moderator</a>
+                    </li>
+                    <li>
+                        <a href="">Privacy Policy</a>
+                    </li>
+                </ul>
+                <ul class="part">
+                    <li>
+                        <h3>Quick links</h3>
+                    </li>
+                    <li>
+                        <a href="">About Us</a>
+                    </li>
+                    <li>
+                        <a href="">Returns And Cancellation</a>
+                    </li>
+                    <li>
+                        <a href="">Sales Coaching</a>
+                    </li>
+                    <li>
+                        <a href="">Speaker/Moderator</a>
+                    </li>
+                    <li>
+                        <a href="">Privacy Policy</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <!-- navigation bar -->
+    <script>
+        var fst_content = document.getElementById("fst_content");
+        var nav_content = document.getElementById("nav-content");
+        var nav_ul = document.getElementById("nav-ul");
+        var menu_icon = document.getElementById("menu-icon");
+        const closeNavbar = document.querySelector(".closeNavigation");
+
+        const menuBtn = document.querySelector(".menu-icon span");
+        const searchBtn = document.querySelector(".search-icon");
+        const cancelBtn = document.querySelector(".cancel-icon");
+        const items = document.querySelector(".nav-items");
+        const form = document.querySelector(".search-box");
+
+        closeNavbar.onclick = function () {
+            menu_icon.classList.remove("activeMenu_icon");
+            nav_ul.classList.remove("activeNav_ul");
+        };
+
+        menu_icon.onclick = function () {
+            menu_icon.classList.toggle("activeMenu_icon");
+            nav_ul.classList.toggle("activeNav_ul");
+            searchBtn.classList.remove("hide");
+            cancelBtn.classList.remove("show");
+            form.classList.remove("active");
+            cancelBtn.style.color = "#ff3d00";
+        };
+
+        cancelBtn.onclick = () => {
+            searchBtn.classList.remove("hide");
+            cancelBtn.classList.remove("show");
+            form.classList.remove("active");
+            cancelBtn.style.color = "#ff3d00";
+        };
+        searchBtn.onclick = () => {
+            form.classList.add("active");
+            searchBtn.classList.add("hide");
+            cancelBtn.classList.add("show");
+            menu_icon.classList.remove("activeMenu_icon");
+            nav_ul.classList.remove("activeNav_ul");
+        };
+        // scrolling effect
+        var nav = document.querySelector("nav");
+        window.addEventListener("scroll", function (e) {
+            const lastPosition = window.scrollY;
+            if (lastPosition > 140) {
+                nav.classList.add("sticky-nav-color");
+                nav.classList.remove("TranskNav");
+            } else if (nav.classList.contains("sticky-nav-color")) {
+                nav.classList.remove("sticky-nav-color");
+                nav.classList.add("TranskNav");
+            } else {
+                nav.classList.remove("sticky-nav-color");
+                nav.classList.add("TranskNav");
+            }
+        });
+    </script>
+
+    <!-- partial -->
+    <script src="https://unpkg.co/gsap@3/dist/gsap.min.js"></script>
+    <script src="https://unpkg.com/gsap@3/dist/ScrollTrigger.min.js"></script>
+    <script src="js/image-gallery.js"></script>
+</body>
+
+</html>
